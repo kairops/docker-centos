984474a2e0d728abcbee05428a6ed48659607861
Pedro Amador Rodríguez <pedroamador.rodriguez@gmail.com>
2018-12-03 07:00:02 +0100

    New: Add java and systemd images

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname $0)/.."
 
-docker build -t redpandaci/docker-centos:base       base
-docker build -t redpandaci/docker-centos:httpd      httpd
-docker build -t redpandaci/docker-centos:jenkins    jenkins
+docker build -t kairops/docker-centos:base       base
+docker build -t kairops/docker-centos:httpd      httpd
+docker build -t kairops/docker-centos:java       java
+docker build -t kairops/docker-centos:jenkins    jenkins
+docker build -t kairops/docker-centos:systemd    systemd
