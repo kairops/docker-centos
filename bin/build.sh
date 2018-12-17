26febd4238e49e4376a7a9bb2862d6ae0218ba1a
Sergio Ortega Gomez <sergio.ortega.gomez@gmail.com>
2018-12-17 09:51:35 +0100

    New: Add redis image

--- bin/build.sh
+++ bin/build.sh
@@ -6,3 +6,4 @@ docker build -t kairops/docker-centos:httpd      httpd
 docker build -t kairops/docker-centos:java       java
 docker build -t kairops/docker-centos:jenkins    jenkins
 docker build -t kairops/docker-centos:systemd    systemd
+docker build -t kairops/docker-centos:redis      redis
